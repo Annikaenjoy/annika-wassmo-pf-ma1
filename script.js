5cef66e2b77d756bbf4531b95a6ef772e7ac7f5e
Annikaenjoy <69652654+Annikaenjoy@users.noreply.github.com>
2020-09-20 22:27:15 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+
+// Module Assignment, Question nr. 1
+var firstVar; 
+
+// nr. 2
+var name = "Annika";
+
+// nr. 3 
+var number = 25;
+
+// nr. 4 
+var devision = (20 / 5);
+console.log(devision);
+
+// nr. 5
+var animal = "frog";
+typeof animal;
+console.log(typeof animal);
+
+// nr. 6
+var orderHasShipped = "true";
+
+// nr. 7 
+if (orderHasShipped = true) {
+    console.log("Your order has been shipped!");
+
+}
+else {
+    console.log ("Your order has not been shipped yet..")
+}
+
+// nr. 8
+for (var i= 0; i<= 9; i++) {
+    console.log(i);
+}
